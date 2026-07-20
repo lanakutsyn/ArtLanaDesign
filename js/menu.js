17cb97636f0e8f85a7b173f2aabcdeb7b4c2c26a
lanakutsyn <skutsyn@wbsedu.de>
2026-07-20 09:05:10 +0200

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,19 @@
+const menuButton = document.querySelector(".menu-toggle");
+const navigation = document.querySelector(".main-navigation");
 
+if (menuButton && navigation) {
+    menuButton.addEventListener("click", () => {
+        const isOpen = navigation.classList.toggle("is-open");
+
+        menuButton.classList.toggle("is-open", isOpen);
+        menuButton.setAttribute("aria-expanded", String(isOpen));
+    });
+
+    navigation.querySelectorAll("a").forEach((link) => {
+        link.addEventListener("click", () => {
+            navigation.classList.remove("is-open");
+            menuButton.classList.remove("is-open");
+            menuButton.setAttribute("aria-expanded", "false");
+        });
+    });
+}
